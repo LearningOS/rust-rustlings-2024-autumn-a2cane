c16cea4bc3c9e6eb8c66406d064e040690f9c5b9
a2cane <2128797698@qq.com>
2024-09-26 11:41:59 +0800

    update

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,14 +19,15 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    
+    // 尝试解析字符串为 i32，并处理错误
+    let qty: i32 = item_quantity.parse()?; // 使用问号运算符直接返回错误
 
     Ok(qty * cost_per_item + processing_fee)
 }
