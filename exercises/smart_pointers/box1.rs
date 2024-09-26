a2be9add5448ea25260993d0aa8636f7ea8bf163
a2cane <2128797698@qq.com>
2024-09-26 12:38:18 +0800

    update

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>), // Step 1: Use Box to allow recursive types
     Nil,
 }
 
@@ -35,11 +33,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil // Step 2: Return an empty list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil)))) // Step 2: Return a non-empty list
 }
 
 #[cfg(test)]
